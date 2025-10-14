6a02215985246540ac6555e52834f0b266de3fbd
paula-nourished <91964207+paula-nourished@users.noreply.github.com>
2025-10-14 12:05:33 +0100

    Update QuizClient.jsx

--- app/QuizClient.jsx
+++ app/QuizClient.jsx
@@ -434,7 +434,7 @@ export default function QuizClient() {
   const [emailGateDone, setEmailGateDone] = useState(false);
   const [emailStatus, setEmailStatus] = useState("idle"); // idle | loading | success | error
   const [emailError, setEmailError] = useState("");
-  const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxSpJreIEkTAKRNysljmC6aaDLkOUlwD0lZYOFii-z5MuZBbdHKhjD-yRbThughCB13/exec";
+  const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyuKrQELcyYked9IVYKOWRakHwDLpJQmGxv-XnATXjLs-sa-hGdNRod6-O56GwTIJ0/exec";
   const isValidEmail = (v) => /^\S+@\S+\.\S+$/.test(v);
 
   // ---- Idle
